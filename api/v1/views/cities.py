faf8aefadd03e4d9dd192be14f51732264d326fa
quduss <shuaibqudus33@gmail.com>
2024-05-05 18:40:18 +0100

    Fix typo in "from models.city import State"

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ objects that handles all default RestFul API actions for cities """
-from models.city import State
-from models.state import City
+from models.state import State
+from models.city import City
 from models import storage
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
